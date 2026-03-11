12480de6ac81fc9075c2ad8b436d682122f389a2
FabioCAlmeida <fabiocezare869@gmail.com>
2026-03-10 23:02:46 -0300

    segundo commit

---
+++ exemplo2_python.py
@@ -0,0 +1,21 @@
+soma = 0
+
+
+num = int(input("Digite um número:")) #funcao int(), converte uma str para int
+soma = soma + num #isto é conhecido como acumulador
+
+num = int(input("Digite um número:"))
+soma = soma + num
+
+
+num = int(input("Digite um número:"))
+soma = soma + num
+
+num = int(input("Digite um número:"))
+soma = soma + num
+
+num = int(input("Digite um número:"))
+soma = soma + num
+print("A soma vale", soma)
+
+int(18.8)
---
+++ exemplo3_python.py
@@ -0,0 +1,7 @@
+altura = float(input("Informe a altura:"))
+base = float(input("Informe a base:"))
+
+
+area = base * altura / 2
+
+print("A area de um triangulo é", area)
